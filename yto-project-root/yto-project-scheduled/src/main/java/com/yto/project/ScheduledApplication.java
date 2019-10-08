801affd72b738dda4351e7917d11e79f1e106068
jianglei <00869712@yto.net.cn>
2019-10-08 14:50:19 +0800

    修改代码

---
+++ yto-project-root/yto-project-scheduled/src/main/java/com/yto/project/ScheduledApplication.java
@@ -0,0 +1,12 @@
+package com.yto.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledApplication.class,args);
+    }
+}
